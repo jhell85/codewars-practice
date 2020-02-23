df50a050cf49df34f69e7605b7dea74315faf872
josh <jhell85@gmail.com>
2020-02-23 12:06:32 -0800

    add new kata

--- src/main.js
+++ src/main.js
@@ -13,16 +13,37 @@ import $ from "jquery";
 // 60 <= score < 70	'D'
 // 0 <= score < 60	'F'
 // Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+// ------------my solution---------------------------------------
+// function getGrade (s1, s2, s3) {
+//   let average = (s1 + s2 + s3)/3;
+//   if( average >= 90){
+//     return "A";
+//   } else if (average >= 80){
+//     return "B";
+//   } else if(average >= 70){
+//     return "C";
+//   } else if(average >= 60){
+//     return "D";
+//   } else {return "F"}
+// }
 
-function getGrade (s1, s2, s3) {
-  let average = (s1 + s2 + s3)/3;
-  if( average >= 90){
-    return "A";
-  } else if (average >= 80){
-    return "B";
-  } else if(average >= 70){
-    return "C";
-  } else if(average >= 60){
-    return "D";
-  } else {return "F";}
-}
+// ---------------------------------------------------------------
+
+// Given: an array containing hashes of names
+
+// Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
+
+// Example:
+
+// list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+// // returns 'Bart, Lisa & Maggie'
+
+// list([ {name: 'Bart'}, {name: 'Lisa'} ])
+// // returns 'Bart & Lisa'
+
+// list([ {name: 'Bart'} ])
+// // returns 'Bart'
+
+// list([])
+// // returns ''
+// Note: all the hashes are pre-validated and will only contain A-Z, a-z, '-' and '.'.
