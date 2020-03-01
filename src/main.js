71bc456d376fc07331f0276147ecedaa2470100b
josh <jhell85@gmail.com>
2020-03-01 09:36:56 -0800

    solved kata

--- src/main.js
+++ src/main.js
@@ -91,7 +91,7 @@ import $ from "jquery";
 
 function bouncingBall(h,  bounce,  window) {
 
-  if(h < 0 || bounce < 0 || bounce > 1 || window > h){
+  if(h < 0 || bounce < 0 || bounce >= 1 || window >= h){
     return -1;
   }
   else{
